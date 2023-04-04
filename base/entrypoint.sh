73f8ceececd73ddac5d0a21f0466fb3cff53838b
cbadenes <cbadenes@gmail.com>
2023-04-04 02:44:48 +0200

    updated openjdk version to 11

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# set JAVA_HOME
+#export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+
 # Set some sensible defaults
 export CORE_CONF_fs_defaultFS=${CORE_CONF_fs_defaultFS:-hdfs://`hostname -f`:8020}
 
